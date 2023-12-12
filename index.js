bdee088509d4b12e684f68f88a875bbfa5df2b93
Elise Erickson <elisece717@gmail.com>
2023-12-12 17:23:04 -0500

    adds initial API fetch for default unsorted data

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadContent();
+})
+
+function loadContent(){
+    fetch("https://ll.thespacedevs.com/2.2.0/spacecraft/?in_space=true")
+    .then(res => res.json())
+    .then(data => renderCards(data))
+}
+
+function renderCards(data){
+    data.results.forEach(craft => {
+        const container = document.createElement("div");
+
+        const name = document.createElement("h2");
+        name.textContent = craft.name
+
+        const body = document.createElement("p");
+        body.textContent = craft.description
+
+        container.appendChild(name);
+        container.appendChild(body);
+
+        const parentContainer = document.getElementById("spacecrafts-container");
+        parentContainer.appendChild(container)
+
+    })
+}
